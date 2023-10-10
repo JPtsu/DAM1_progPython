ecc7ddbf49efcffb5c0b97a73f09e2670dfbd4f2
rmungar1209 <rmungar1298@g.educaand.es>
2023-10-10 19:45:01 +0200

    30 terminado

--- Ejercicios UD1/Ejercicio 30.py
+++ Ejercicios UD1/Ejercicio 30.py
@@ -1,20 +1,13 @@
 # Escribe: "Dame un número inicial, un incremento y un número final"
 # lee numI, inc, numF
-# si inc = 0 y numF =0: 
-#   
-#
-#
-#
-#
-#
-#
-#
-#
+# crea una lista desde numI hasta numF
+# Escribe lista  
 #
 numI = int(input("Dame un número inicial:"))
 inc = int(input("Dame un incremento:"))
 numF = int(input("Dame un número final:"))
-serie= list()
+serie= list(range(numI-1,numF+1))
+serie= serie[numI:numF:inc]
 print(serie)
 
     
