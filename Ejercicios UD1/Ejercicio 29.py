12cfb28118f2e233f8471f940032afcabdaa1744
rmungar1209 <rmungar1298@g.educaand.es>
2023-10-11 16:49:26 +0200

    terminado 29

--- Ejercicios UD1/Ejercicio 29.py
+++ Ejercicios UD1/Ejercicio 29.py
@@ -4,17 +4,18 @@
 #   nombre= "DESCONOCIDO"
 # si edad<0 o 125<edad:
 #   Escribe: "Error"
+#   Escribe: "Vuelve a escribir tu edad:"
+#   edad=edad
 # sino
 #   Escribe: "Te llamas (nombre) y tienes (edad) años, te quedan aun (125-edad) años por cumplir"
 #
 nombre = input("Dame un nombre:")
 edad = int(input("Dime la edad:"))
-añosR= (125-edad)
 if len(nombre)==0:
     nombre = "DESCONOCIDO"
 else:
     nombre = nombre
-if edad<0 or 125<edad:
+while edad<0 or 125<edad:
     print("ERROR")
-else:
-    print("Te llamas",nombre,"y tienes",edad,"años, te quedan aún",añosR,"años por cumplir")
+    edad= int(input(("Vuelve a escribir tu edad:")))
+print("Te llamas",nombre,"y tienes",edad,"años, te quedan aún",(125-edad),"años por cumplir")   
