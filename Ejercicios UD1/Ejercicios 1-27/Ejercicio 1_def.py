dfb313228fc6665fee878e46dbd22f8facd8c45d
rmungar1209 <rmungar1298@g.educaand.es>
2023-10-16 21:35:41 +0200

    _def

---
+++ Ejercicios UD1/Ejercicios 1-27/Ejercicio 1_def.py
@@ -0,0 +1,5 @@
+nombre = input("Dime tu nombre: ")
+def saludo():
+    return ("Hola, " + nombre + ".")
+print (saludo())
+
---
+++ Ejercicios UD1/Ejercicios 1-27/Ejercicio 2_def.py
@@ -0,0 +1,6 @@
+
+horas =int(input("Horas de trabajo:"))
+coste = int(input("Coste por hora:"))
+def salario():
+    return (horas*coste)
+print (salario())
